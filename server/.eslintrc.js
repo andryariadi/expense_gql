1cf173b041ad74eaf7bc95ffc1bdf999d847e3d3
Andry Ariadi <andryariadi23@gmail.com>
2025-02-22 06:46:42 +0700

    setup graphql

---
+++ server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    // aturan ESLint lainnya...
+  },
+};
