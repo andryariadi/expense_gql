0dcd84ed2d2c4facaa7ec58c7f099000e5e5ae43
Andry Ariadi <andryariadi23@gmail.com>
2025-02-22 14:45:52 +0700

    setup expressjs in graphql server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,61 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
-import mergedTypeDefs from "./typeDefs.js";
-import mergedResolvers from "./resolvers.js";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
+
+import mergedTypeDefs from "./typeDefs/index.js";
+import mergedResolvers from "./resolvers/index.js";
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start(); // Ensure we wait for our server to start
 
-console.log(`🚀 Server ready at ${url}`);
+// Set up our Express middleware to handle CORS, body parsing and our expressMiddleware function.
+app.use(
+  "/graphql",
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 5000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:5000/graphql`);
+
+// without express.js
+// const server = new ApolloServer({
+//     typeDefs: mergedTypeDefs,
+//     resolvers: mergedResolvers,
+//   });
+
+//   const { url } = await startStandaloneServer(server, {
+//     listen: { port: 5000 },
+//   });
+
+//   console.log(`🚀 Server ready at ${url}`);
--- server/resolvers/user.resolver.js
+++ server/resolvers/user.resolver.js
@@ -4,7 +4,10 @@ const userResolver = {
   Query: {
     users: () => {
       return users;
-    },
+    }, // name of the query users must match the name of the query in the user.typeDef.js and Appolo server
+    user: (_, { userId }) => {
+      return users.find((user) => userId === user._id);
+    }, // args userId must match the name of the query in the user.typeDef.js and Appolo server
   },
 
   Mutation: {},
