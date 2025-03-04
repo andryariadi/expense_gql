75a83c964c5ec21e18929e4ce274da93c1ca0b41
Andry Ariadi <andryariadi23@gmail.com>
2025-03-04 07:31:38 +0700

    create user query client side

--- client/src/libs/apolloClient.ts
+++ client/src/libs/apolloClient.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: " http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  uri: " http://localhost:5000/graphql", // this is the URL of the server
+  cache: new InMemoryCache(), // this is the cache
+  credentials: "include", // this is to include the credentials in the request headers
 });
